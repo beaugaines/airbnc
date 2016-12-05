0a036c2c6d48bfd6ad37682d9f66dd5dd3c06cf5
Charlie Gaines <beaugaines@gmail.com>
2016-12-05 11:15:46 -0500

    added seed data for rooms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(
+  first_name: 'Bob',
+  last_name: 'Smith',
+  email: 'bob@hotmail.com',
+  password: 'password'
+)
+
+10.times do
+  Room.create(
+    user: u,
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    city: Faker::Address.city,
+    country: Faker::Address.country
+  )
+end
