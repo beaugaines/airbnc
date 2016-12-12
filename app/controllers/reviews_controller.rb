34e78d8209e9c1269efa3a08add895f054063ecf
Yen <yenchieh86@hotmail.com>
2016-12-12 09:29:11 +0000

    add review's create/delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,34 @@
 class ReviewsController < ApplicationController
-    
-    def index
-        @reviews = Review.all
+    def create
+        @room = Room.find(params[:room_id])
+        review = @room.reviews.new(review_params)
+        review.user = current_user
+        if review.save
+            flash[:notice] = "Your review have been saved"
+            redirect_to @room
+        else
+            flash[:alert] = "There's an error, please try again"
+            render @room
+        end
     end
     
-    def show
-        @reviews = Review.find[:rooms][:title]
+    def destroy
+        @room = Room.find(params[:room_id])
+        review = @room.reviews.find(params[:id])
+        
+        if review.destroy
+            flash[:notice] = "Review have been deleted"
+            redirect_to @room
+        else
+            flash[:alert] = "Comment couldn't be deleted. Try again."
+            redirect_to @room
+        end
     end
+    
+     private
+    
+    def review_params
+        params.require(:review).permit(:body)
+    end
+    
 end
