9c23521c481a49ddf4f54c4185b5b79d24aa4be7
Charlie Gaines <beaugaines@gmail.com>
2016-12-23 11:50:40 -0500

    refactor search controller for fuzzy room search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
     def index
-        if found = Room.find_by(title: params[:keyword]) || User.find_by(email: params[:keyword])
+        query_param = "%#{params[:keyword]}%"
+        if found = Room.where('lower(title) LIKE ?', query_param).first || User.find_by(email: params[:keyword])
             redirect_to found
         end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220024232) do
+ActiveRecord::Schema.define(version: 20161223163352) do
 
   create_table "helpfuls", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,9 +36,10 @@ ActiveRecord::Schema.define(version: 20161220024232) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "room_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.text     "body"
+    t.integer  "helpful_count", default: 0
     t.index ["room_id"], name: "index_reviews_on_room_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
